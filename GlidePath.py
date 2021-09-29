fac104beb8f3c005ca22075de86a0d5c88b9bc8a
ja-55 <87917798+ja-55@users.noreply.github.com>
2021-09-28 20:08:23 -0500

    Update GlidePath.py

--- GlidePath.py
+++ GlidePath.py
@@ -29,9 +29,59 @@ dates = pd.date_range(start = beg_dt, freq = frq, end = end_dt)
 
 # ASSUMPTIONS: OTHER
 sims = 101
-cols = ['BB','Con_Base','Con_Inc','Con_Oth','Returns','EB']
+cols = ['BB','Contrib','Returns','EB']
 plt_lgnd = ['Median','10th Percentile','90th Percentile']
 
 # EMPTY DATAFRAMES
 eb_df = pd.DataFrame(index = dates)
 stat_df = pd.DataFrame(index = dates)
+
+
+
+# FILL DATAFRAMES
+
+for sim in range(sims):
+    
+    temp_df = pd.DataFrame(columns = cols, index = dates)
+    exp_ret = pd.DataFrame(np.random.normal(rtn_mu, rtn_sgma, pds), index = dates)
+    
+    for index, row in temp_df.iterrows():
+        if index == beg_date:
+            row['BB'] = 0
+            row['Contrib'] = bal_init + contrib
+            row['Returns'] = (row['BB'] + row['Contrib'] / 2) * (exp_ret.loc[index,:].values[0] / 12)
+            row['EB'] = (row['BB'] + row['Contrib'] + row['Returns'])
+        else:
+            row['BB'] = temp_df.shift(1).loc[index,'EB']
+            row['Contrib'] = contrib
+            row['Returns'] = (row['BB'] + row['Contrib'] / 2) * (exp_ret.loc[index,:].values[0] / 12)
+            row['EB'] = (row['BB'] + row['Contrib'] + row['Returns'])
+    eb_df.loc[:,sim] = temp_df['EB']
+
+# FIND MEDIAN, 90TH PERCENTILE, AND 10TH PERCENTILE
+col_med = eb_df.loc[end_dt,:].loc[eb_df.loc[end_dt,:] == eb_df.loc[end_dt,:].median()].index[0]
+col_90p = eb_df.loc[end_dt,:].loc[eb_df.loc[end_dt,:] == eb_df.loc[end_dt,:].quantile(q = 0.9)].index[0]
+col_10p = eb_df.loc[end_dt,:].loc[eb_df.loc[end_dt,:] == eb_df.loc[end_dt,:].quantile(q = 0.1)].index[0]
+
+# STORE MEDIAN, 90TH PERCENTILE, AND 10TH PERCENTILE IN DATAFRAME
+stat_df = eb_df.loc[:,[col_med, col_90p, col_10p]]
+
+# PLOT GLIDEPATH
+plt.close()
+fig, ax = plt.subplots(1,1, figsize = (15,7))
+stat_df.plot.line(ax = ax, legend = True).legend(plt_lgnd, fontsize = 14)
+plt.scatter([dt_24] * 3, stat_df.loc[dt_24,:].values, marker = 'o', c = 'red', s = 50)
+plt.scatter([dt_26] * 3, stat_df.loc[dt_26,:].values, marker = 'o', c = 'blue', s = 50)
+plt.scatter([end_dt] * 3, stat_df.loc[end_dt,:].values, marker = 'o', c = 'black', s = 50)
+plt.title('Figure 1:Projected Portfolio Balance', fontweight = 'bold', fontsize = 15)
+plt.ylabel('Projected Wealth ($)', fontsize = 14)
+plt.xticks(fontsize = 14)
+plt.yticks(fontsize = 14)
+ax.set_xlim([beg_dt, end_dt_plt])
+
+fmt = '${x:,.0f}'
+tick = mtick.StrMethodFormatter(fmt)
+plt.gca().yaxis.set_major_formatter(tick)
+
+# DISPLAY BALANCES AS OF KEY DATES
+print(stat_df.loc[[dt_yr5,dt_yr10,dt_yr20],:])
